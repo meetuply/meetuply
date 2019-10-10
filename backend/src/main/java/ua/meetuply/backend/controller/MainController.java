fa5f404a3ef8760a4b0690b74d4ae7d4deca1348
Mariia Malitska <37335222+MariiaMalitska@users.noreply.github.com>
2019-10-10 13:57:34 +0300

    Divided main controller and blog controller

--- backend/src/main/java/ua/meetuply/backend/controller/MainController.java
+++ backend/src/main/java/ua/meetuply/backend/controller/MainController.java
@@ -31,6 +31,9 @@ public class MainController {
  
    @Autowired
    private AppUserValidator appUserValidator;
+
+   @Autowired
+   private BlogPostValidator blogPostValidator;
  
    // Set a form validator
    @InitBinder
@@ -110,81 +113,6 @@ public class MainController {
  
       redirectAttributes.addFlashAttribute("flashUser", newUser);
       return "redirect:/registerSuccessful";
-   }
-
-   //Blog section
-
-   @Autowired
-   private BlogPostDAO blogPostDAO;
-
-   @Autowired
-   private BlogPostValidator blogPostValidator;
-
-   @RequestMapping("/blogPosts")
-   public String viewBlogPosts(Model model) {
-
-      List<BlogPost> list = blogPostDAO.getBlogPosts();
-      model.addAttribute("blogPosts", list);
-      return "blog/listBlogPostsPage";
-   }
-
-   @RequestMapping(value = "/newBlogPost", method = RequestMethod.GET)
-   public String viewNewBlogPost(Model model) {
-      BlogPostForm form = new BlogPostForm();
-      model.addAttribute("blogPostForm", form);
-      return "blog/newBlogPostPage";
-   }
-
-   @RequestMapping(value = "/newBlogPost", method = RequestMethod.POST)
-   public String saveNewBlogPost(Model model, //
-                                 @ModelAttribute("blogPostForm") @Validated BlogPostForm blogPostForm, //
-                                 BindingResult result, //
-                                 final RedirectAttributes redirectAttributes) {
-
-      // Validate result
-      if (result.hasErrors()) {
-         return "blog/newBlogPostPage";
-      }
-      BlogPost bp= null;
-      try {
-         bp = blogPostDAO.createBlogPost(blogPostForm);
-      }
-      catch (Exception e) {
-         model.addAttribute("errorMessage", "Error: " + e.getMessage());
-         return "blog/newBlogPostPage";
-      }
-
-      redirectAttributes.addFlashAttribute("flashBlogPost", bp);
-
-      return "redirect:/blogPosts";
-   }
-
-//   @RequestMapping(value = "/email", method = RequestMethod.GET)
-//   @ResponseBody
-//   public String currentUserName(Principal principal) {
-//      return principal.getName();
-//   }
-
-
-   //Blog comment section
-
-   @Autowired
-   private BlogCommentDAO blogCommentDAO;
-
-   @Autowired
-   private BlogCommentValidator blogCommentValidator;
-
-   @RequestMapping(value = "/postComment/{postid}", method = RequestMethod.GET)
-   public String viewCurrentBlogPost(@PathVariable("postid") long postid, Model model) {
-      BlogPost post = blogPostDAO.getBlogPostById(postid);
-      model.addAttribute("blogPost", post);
-
-      List<BlogComment> list = blogCommentDAO.findBlogCommentsByPost(post);
-      model.addAttribute("blogComments", list);
-
-      BlogCommentForm form = new BlogCommentForm();
-      model.addAttribute("blogCommentForm", form);
-      return "blog/currentBlogPostPage";
    }
 
 
