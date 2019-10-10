bc2c9f6618c5e86cfe0e8e4b2af2ea84c0da2822
Mariia Malitska <37335222+MariiaMalitska@users.noreply.github.com>
2019-10-10 15:43:40 +0300

    Update MainController.java

--- backend/src/main/java/ua/meetuply/backend/controller/MainController.java
+++ backend/src/main/java/ua/meetuply/backend/controller/MainController.java
@@ -1,27 +1,8 @@
 package ua.meetuply.backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import ua.meetuply.backend.dao.AppUserDAO;
-import ua.meetuply.backend.dao.BlogCommentDAO;
-import ua.meetuply.backend.dao.BlogPostDAO;
-import ua.meetuply.backend.formbean.AppUserForm;
-import ua.meetuply.backend.formbean.BlogCommentForm;
-import ua.meetuply.backend.formbean.BlogPostForm;
-import ua.meetuply.backend.model.AppUser;
-import ua.meetuply.backend.model.BlogComment;
-import ua.meetuply.backend.model.BlogPost;
-import ua.meetuply.backend.validator.AppUserValidator;
-import ua.meetuply.backend.validator.BlogCommentValidator;
-import ua.meetuply.backend.validator.BlogPostValidator;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
