e2237287580dfe5d5cb0a5b486d0e36498c8392e
tetianaChernova <chtania.2000@gmail.com>
2019-10-13 23:30:01 +0300

    Integration of emailService with AppUserController.
    Modified greeting template
    Configured Freemarker
    Refactored EmailService
    Added email credentials to application.properties

---
+++ backend/src/main/java/ua/meetuply/backend/config/FreemarkerConfig.java
@@ -0,0 +1,16 @@
+package ua.meetuply.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+
+@Configuration
+public class FreemarkerConfig {
+
+    @Bean
+    public FreeMarkerConfigurationFactoryBean getFreeMarkerConfiguration() {
+        FreeMarkerConfigurationFactoryBean fmConfigFactoryBean = new FreeMarkerConfigurationFactoryBean();
+        return fmConfigFactoryBean;
+    }
+
+}
