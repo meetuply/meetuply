edcc92c77851f23c63eaf766aa9469752ea023bb
San Nguyen <mr.sn5.kma@gmail.com>
2019-10-18 19:40:11 +0300

    Update AppUserValidator.java

--- backend/src/main/java/ua/meetuply/backend/validator/AppUserValidator.java
+++ backend/src/main/java/ua/meetuply/backend/validator/AppUserValidator.java
@@ -34,7 +34,7 @@ public class AppUserValidator implements Validator {
         if (appUser.getEmail().length() > 200) errors.rejectValue("email", "Length.appUser.email", "Email address is too long");
         if (appUser.getFirstName().length() > 100) errors.rejectValue("name", "Length.appUser.email", "Email address is too long");
         if (appUser.getLastName().length() > 100) errors.rejectValue("surname", "Length.appUser.email", "Email address is too long");
-        if (appUser.getPassword().length() < 73) errors.rejectValue("password", "Length.appUser.password", "Password is too long");
+        if (appUser.getPassword().length() > 73) errors.rejectValue("password", "Length.appUser.password", "Password is too long");
 
         if (!this.emailValidator.isValid(appUser.getEmail())) {
             errors.rejectValue("email", "Pattern.appUser.email", "Not valid email");
