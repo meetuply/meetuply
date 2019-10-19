b58dbe060cb4915ef3a7d31243729cdb0b9cd3e3
Khrystyna12 <Khrystynka.Sem@gmail.com>
2019-10-20 02:14:03 +0300

    Granted Authority admin

--- backend/src/main/java/ua/meetuply/backend/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/ua/meetuply/backend/service/UserDetailsServiceImpl.java
@@ -1,9 +1,12 @@
 package ua.meetuply.backend.service;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,8 +24,14 @@ public class UserDetailsServiceImpl implements UserDetailsService{
     @Override
 
     public UserDetails loadUserByUsername(String email) {
-        AppUser user = appUserDAO.findAppUserByEmail(email);
-        if (user == null) throw new UsernameNotFoundException(email);
-        return new User(user.getEmail(), user.getPassword(), new HashSet<>());
+        AppUser appUser = appUserDAO.findAppUserByEmail(email);
+        if (appUser == null) throw new UsernameNotFoundException(email);
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        User user;
+        if(appUser.getRole().getRoleName().equals("admin"))
+            user = new User(appUser.getEmail(), appUser.getPassword(), grantedAuthorities);
+        else user = new User(appUser.getEmail(), appUser.getPassword(), new HashSet<>());
+        return user;
     }
 }
