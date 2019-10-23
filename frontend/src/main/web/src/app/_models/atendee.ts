1503bea941df13f9db587e3fe9afc702b84cc31a
Kate-Dt <dotsiuk.k@gmail.com>
2019-10-24 00:55:03 +0300

    added user rating on meetups pages

--- frontend/src/main/web/src/app/_models/atendee.ts
+++ frontend/src/main/web/src/app/_models/atendee.ts
@@ -1,6 +1,22 @@
+import {RatingService} from "../_services/rating.service";
+
 export class Atendee {
+  id: number;
 	name: string;
 	surname: string;
 	rating: number;
 	photo: string;
+
+	constructor(id:number, name: string, surname: string, photo: string,
+              private ratingService: RatingService){
+	  this.id = id;
+	  this.name = name;
+	  this.surname = surname;
+	  this.photo = photo;
+	  this.ratingService.getUserRatingAvg(this.id).subscribe(
+	    avgRating => {
+	      this.rating = avgRating;
+      }
+    )
+  }
 }
