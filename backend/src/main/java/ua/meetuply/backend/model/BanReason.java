b1274f9ee8c5b7c9e2d5675e696777208d21b673
Khrystyna12 <Khrystynka.Sem@gmail.com>
2019-11-03 00:05:15 +0200

    Refactored topics and bans on backend

--- backend/src/main/java/ua/meetuply/backend/model/BanReason.java
+++ backend/src/main/java/ua/meetuply/backend/model/BanReason.java
@@ -1,28 +1,16 @@
 package ua.meetuply.backend.model;
 
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BanReason {
-	
-	public BanReason() { }
-	
-	public BanReason(Integer banReasonId, String name) {
-		this.banReasonId = banReasonId;
-		this.name = name;
-	}
 	
 	private Integer banReasonId;
 	private String name;
-	
-	public Integer getBanReasonId() {
-		return banReasonId;
-	}
-	public void setBanReasonId(Integer banReasonId) {
-		this.banReasonId = banReasonId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
