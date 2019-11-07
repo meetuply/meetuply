e9916783f36d936fe6aaaddcf9a534755dcaa93a
tetianaChernova <chtania.2000@gmail.com>
2019-11-07 14:00:38 +0200

    Basic Filter Front

--- frontend/src/main/web/src/app/app.module.ts
+++ frontend/src/main/web/src/app/app.module.ts
@@ -64,7 +64,8 @@ import {DashboardPageComponent} from "./dashboard-page/dashboard-page.component"
 import {FeedbackCreatePageComponent} from "./feedback-create-page/feedback-create-page.component";
 import { BanPageComponent } from './ban-page/ban-page.component';
 import { BansPageComponent } from './bans-page/bans-page.component';
-
+import {MatNativeDateModule} from "@angular/material";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -136,7 +137,9 @@ import { BansPageComponent } from './bans-page/bans-page.component';
     MatFormFieldModule,
     MatDatepickerModule,
     MatInputModule,
-    MatMomentDateModule
+    MatMomentDateModule,
+    MatNativeDateModule,
+    NgbModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
