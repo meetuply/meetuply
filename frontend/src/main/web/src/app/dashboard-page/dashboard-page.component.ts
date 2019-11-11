9699d78b7aa5cebd3eb8cf3ebe84f80c1c259b1d
Mariia Malitska <37335222+MariiaMalitska@users.noreply.github.com>
2019-11-11 22:21:37 +0200

    dashboard functionality added

--- frontend/src/main/web/src/app/dashboard-page/dashboard-page.component.ts
+++ frontend/src/main/web/src/app/dashboard-page/dashboard-page.component.ts
@@ -8,6 +8,9 @@ import {BlogListItem} from "../_models/blogListItem";
 import {BlogService} from "../_services/blog.service";
 import {StateService} from "../_services/state.service";
 import {MeetupListItem} from "../_models/meetupListItem";
+import {SpeakerListItem} from "../_models/speakerListItem";
+import {FeedbackService} from "../_services/feedback.service";
+import {User} from "../_models";
 
 
 @Component({
@@ -22,9 +25,11 @@ export class DashboardPageComponent implements OnInit {
   soonMeetups: MeetupListItem[] = [];
   userMeetups: Meetup[] = [];
   lastNotifications = [];
+  speakers: User[]=[];
   blogPosts: BlogListItem[] = [];
   viewAllSoon = false;
   viewAllUser = false;
+  viewAllSpeakers = false;
   error;
   private sub: Subscription;
 
@@ -32,13 +37,15 @@ export class DashboardPageComponent implements OnInit {
               public userService: UserService,
               private meetupService: MeetupService,
               private blogService: BlogService,
-              public stateService: StateService) {
+              public stateService: StateService,
+              public feedbackService: FeedbackService) {
   }
 
   ngOnInit() {
     this.id = this.userService.currentUser.userId;
     this.loadBlogPosts();
     this.loadMeetups();
+    this.loadSpeakers();
   }
 
   loadMeetups(){
@@ -86,12 +93,50 @@ export class DashboardPageComponent implements OnInit {
     );
   }
 
+  loadSpeakers() {
+    this.sub = this.feedbackService.getWaitingFeedback(this.id).subscribe(
+      async data => {
+        if (data) {
+          this.speakers = await Promise.all(data.map(async id => {
+            let firstName = "";
+            let lastName = "";
+            let photo = "";
+            let speakerId = 0;
+            await this.userService.get(id).toPromise().then(
+              speaker => {
+                firstName = speaker.firstName;
+                lastName = speaker.lastName;
+                photo = speaker.photo;
+                speakerId = speaker.userId;
+              }
+            );
+            let speaker = new User();
+            speaker.userId=speakerId;
+            speaker.firstName=firstName;
+            speaker.lastName = lastName;
+            speaker.photo = photo;
+            return speaker;
+            }
+          ));
+          console.log(this.blogPosts);
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
   changeViewAllSoon(){
     this.viewAllSoon = !this.viewAllSoon;
   }
 
   changeViewAllUser(){
     this.viewAllUser = !this.viewAllUser;
+  }
+
+  changeViewAllSpeakers(){
+    this.viewAllSpeakers = !this.viewAllSpeakers;
   }
 
   ngOnDestroy() {
